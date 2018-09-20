55c5902be612c459a73e615f2eafb6ac0d622a91
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-09-20 16:12:38 +0100

    tetris-6 Added preview window

--- src/main/scala/Spawner.scala
+++ src/main/scala/Spawner.scala
@@ -2,47 +2,44 @@ import scala.util.Random
 
 object Spawner {
 
+
   def spawn ={
     Random.nextInt(4) match {
       case 0 => spawnLShape
-      case 1 => spawnLineHoriz
-      case 2 => spawnTShape
+      case 1 => spawnTShape
+      case 2 => spawnLineVers
       case _ => spawnSquare
     }
   }
 
   def spawnSquare={
     val colour = generateColour
-    List(new Tetromino("square", colour, 3),new Tetromino("square", colour, 4),
-      new Tetromino("square", colour, 14),new Tetromino("square", colour, 13))
+    List(
+      new Tetromino("square", colour,-26),new Tetromino("square", colour, -25),
+      new Tetromino("square", colour, -16),new Tetromino("square", colour, -15))
   }
 
   def spawnTShape ={
     val colour = generateColour
-    List(
-      new Tetromino("Tshape", colour, 3),
-      new Tetromino("Tshape", colour, 4),
-      new Tetromino("Tshape", colour, 14),
-      new Tetromino("Tshape", colour, 5))
+    List(                                   new Tetromino("Tshape", colour, -36),
+      new Tetromino("Tshape", colour, -27), new Tetromino("Tshape", colour, -26), new Tetromino("Tshape", colour, -25))
   }
 
-
-  def spawnLineHoriz ={
+  def spawnLineVers ={
     val colour = generateColour
     List(
-      new Tetromino("lineHor", colour, 3),
-      new Tetromino("lineHor", colour, 4),
-      new Tetromino("lineHor", colour, 5),
-      new Tetromino("lineHor", colour, 6))
+      new Tetromino("lineVer", colour, -36),
+      new Tetromino("lineVer", colour, -26),
+      new Tetromino("lineVer", colour, -16),
+      new Tetromino("lineVer", colour, -6))
   }
 
   def spawnLShape ={
     val colour = generateColour
     List(
-      new Tetromino("L", colour, 5),
-      new Tetromino("L", colour, 15),
-      new Tetromino("L", colour, 25),
-      new Tetromino("L", colour, 26))
+      new Tetromino("L", colour, -37), new Tetromino("L", colour, -36),
+                                       new Tetromino("L", colour, -26),
+                                       new Tetromino("L", colour, -16))
   }
 
   def generateColour: String ={
