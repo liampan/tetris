a4c200699f06c0d1b739b74254f7fe8117ee82d3
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-09-20 16:15:04 +0100

    fixed window flip

--- src/main/scala/reader.scala
+++ src/main/scala/reader.scala
@@ -141,7 +141,7 @@ object reader {
     println()
     println(" Up next:")
     val preview = nextTet
-    val spawnPos = List(-4,-5,-6,-7,-14,-15,-16,-17,-24,-25,-26,-27,-34,-35,-36,-37)
+    val spawnPos = List(-4,-5,-6,-7,-14,-15,-16,-17,-24,-25,-26,-27,-34,-35,-36,-37).reverse
     val ind: List[List[String]] = spawnPos.map(i => if(nextTet.map(_.getIndex).contains(i)) "■" else " ").grouped(4).toList
 
     println("┏━" + "━"*8 + "━┓")
