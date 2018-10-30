9c41a7563a1e411e8e9e19eed1e79fd77378b2e4
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-10-30 17:19:23 +0000

    tet-12 | refactor

--- src/main/scala/Spawner.scala
+++ src/main/scala/Spawner.scala
@@ -11,43 +11,43 @@ object Spawner{
   private def spawnSShape = {
     val colour = generateColour
     val indexes = List(-37, -27, -26, -16)
-    indexes.map(new Tetromino(sShape, colour, _))
+    indexes.map(Tetromino(sShape, colour, _))
   }
 
   private def spawnZShape = {
     val colour = generateColour
     val indexes = List(-36, -27, -26, -17)
-    indexes.map(new Tetromino(zShape, colour, _))
+    indexes.map(Tetromino(zShape, colour, _))
   }
 
   private def spawnSquare = {
     val colour = generateColour
     val indexes = List(-26, -25, -16, -15)
-    indexes.map(new Tetromino(square, colour, _))
+    indexes.map(Tetromino(square, colour, _))
   }
 
   private def spawnTShape = {
     val colour = generateColour
     val indexes = List(-36, -27, -26, -25)
-    indexes.map(new Tetromino(tee, colour, _))
+    indexes.map(Tetromino(tee, colour, _))
   }
 
   private def spawnLine = {
     val colour = generateColour
     val indexes = List(-36, -26, -16, -6)
-    indexes.map(new Tetromino(line, colour, _))
+    indexes.map(Tetromino(line, colour, _))
   }
 
   private def spawnLShape = {
     val colour = generateColour
     val indexes = List(-37, -36, -26, -16)
-    indexes.map(new Tetromino(lShape, colour, _))
+    indexes.map(Tetromino(lShape, colour, _))
   }
 
   private def spawnJShape = {
     val colour = generateColour
     val indexes = List(-36, -35, -26, -16)
-    indexes.map(new Tetromino(jShape, colour, _))
+    indexes.map(Tetromino(jShape, colour, _))
   }
 
   private def generateColour: String = {
